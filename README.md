77247328e9acc7c4adb6a567cf0be231649fb25b
Dev Goel <goeldev01@gmail.com>
2023-01-12 01:29:39 +0530

    Added lex-yacc implementation, finished compiler design steps

--- README.md
+++ README.md
@@ -9,3 +9,26 @@ More about general compiler design than following the footsteps of Lox, with som
 2. **Parsing** - Takes the flat sequence of tokens generated and builds a 'parse tree', or an 'abstract syntax tree', or simply 'syntax tree'.
 
 3. **Static Analysis** - The first bit of analysis that most languages do is called 'binding' or 'resolution'. If the language is statically typed, we can figure out types, and probably store them back as attributes in the AST. Or, transform it into a symbol table, or into an entirely different data structure.
+
+4. **Intermediate representation** - The code can be stored in some form of shared intermediate form, i.e., not tightly tied to either the source or destination.
+
+5. **Optimization** - Optimization is a huge part of the programming language. Uses methods like constant folding to optimize.
+
+6. **Code generation** - Convert it all to a form which the machine can actually run. Now, here comes the decision, do we generate instructions for a real CPU or a virtual one? Native code is fast, but generation of that code is complicated. Another way is producing VM code, which we generally refer to as 'bytecode' today.
+
+7. **Virtual Machine** - If producing bytecode, you can write a VM which will run your bytecode.
+
+8. **Runtime**
+
+- We finally hammered the user's program into a form that we can execute. If compiled to machine code, we simply tell the OS to load the executable. If compiled to bytecode, we need to start the VM and load the program into that.
+- In a fully compiled language, like Go, the code implementing the runtime gets inserted directly into the resulting executable. If running inside a VM, then the runtime also lives there. Like in Java, Python and Javascript.
+
+## Shortcut & Alternate Routes
+
+1. Single-pass compilers = produce output code directly in parser, without any ASTs or IR. This means you need to provide all info for correct compilation at declaration only. Examples - Pascal and C. **Note** - This is why C requires linear declaration of functions as per their usage, and not in a random order.
+
+2. Tree-walk interpreters = Begin execution after parsing to AST.
+
+3. Transpilers
+
+4. JIT -
--- lex-yacc/lex.yy.c
+++ lex-yacc/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 10
+#define YY_END_OF_BUFFER 11
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,28 +360,30 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[18] =
+static const flex_int16_t yy_accept[39] =
     {   0,
-        0,    0,    4,    2,    3,    2,    0,    0,    0,    0,
-        0,    0,    0,    0,    0,    1,    0
+        0,    0,   11,    9,    8,    8,    7,    1,    6,    6,
+        7,    6,    6,    6,    6,    6,    6,    6,    6,    6,
+        6,    6,    6,    6,    6,    3,    5,    6,    2,    6,
+        6,    6,    6,    6,    6,    6,    4,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    4,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    4,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    6,    1,    1,
+        1,    1,    1,    1,    7,    7,    7,    7,    7,    7,
+        7,    7,    7,    7,    7,    7,    7,    7,    7,    7,
+        7,    7,    7,    7,    7,    7,    7,    7,    7,    7,
+        1,    1,    1,    1,    8,    1,    9,   10,    7,   11,
 
-        5,    1,    1,    6,    1,    1,    1,    7,    1,    1,
-        8,    1,    1,    9,    1,    1,    1,    1,   10,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+       12,   13,    7,    7,   14,    7,    7,   15,   16,   17,
+       18,   19,    7,   20,    7,   21,    7,    7,    7,   22,
+       23,    7,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,35 +400,51 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[11] =
+static const YY_CHAR yy_meta[24] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1
+        1,    1,    1,    2,    2,    1,    2,    2,    2,    2,
+        2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
+        2,    2,    2
     } ;
 
-static const flex_int16_t yy_base[19] =
+static const flex_int16_t yy_base[41] =
     {   0,
-        0,    5,   18,   19,   19,   12,    9,    8,    6,   10,
-        2,    2,    0,    1,    0,   19,   19,    0
+        0,   11,   49,   50,   50,   50,    5,   50,    0,   38,
+       16,    0,   39,    6,   37,   27,    9,   28,   23,   32,
+       22,   28,   18,   23,   25,    0,    0,   24,    0,   27,
+       12,   10,   17,   15,   12,    2,    0,   50,   32,    5
     } ;
 
-static const flex_int16_t yy_def[19] =
+static const flex_int16_t yy_def[41] =
     {   0,
-       18,   18,   17,   17,   17,   17,   17,   17,   17,   17,
-       17,   17,   17,   17,   17,   17,    0,   17
+       39,   39,   38,   38,   38,   38,   38,   38,   40,   40,
+       38,   40,   40,   40,   40,   40,   40,   40,   40,   40,
+       40,   40,   40,   40,   40,   40,   40,   40,   40,   40,
+       40,   40,   40,   40,   40,   40,   40,    0,   38,   38
     } ;
 
-static const flex_int16_t yy_nxt[30] =
+static const flex_int16_t yy_nxt[74] =
     {   0,
-        4,    5,   17,   16,   17,    6,    5,   15,   14,   13,
-        6,   12,   11,   10,    9,    8,    7,   17,    3,   17,
-       17,   17,   17,   17,   17,   17,   17,   17,   17
+        4,    5,    6,    4,    7,    8,   12,    4,   11,   11,
+       10,    4,    5,    6,    4,    7,    8,   20,    4,   11,
+       11,   10,   15,   37,   16,   36,   17,   35,   34,   33,
+       32,   21,    9,    9,   31,   30,   29,   28,   27,   26,
+       25,   24,   23,   22,   19,   18,   14,   13,   38,    3,
+       38,   38,   38,   38,   38,   38,   38,   38,   38,   38,
+       38,   38,   38,   38,   38,   38,   38,   38,   38,   38,
+       38,   38,   38
     } ;
 
-static const flex_int16_t yy_chk[30] =
+static const flex_int16_t yy_chk[74] =
     {   0,
-       18,    1,    0,   15,    0,    1,    2,   14,   13,   12,
-        2,   11,   10,    9,    8,    7,    6,    3,   17,   17,
-       17,   17,   17,   17,   17,   17,   17,   17,   17
+        1,    1,    1,    1,    1,    1,   40,    1,    7,    7,
+        1,    2,    2,    2,    2,    2,    2,   17,    2,   11,
+       11,    2,   14,   36,   14,   35,   14,   34,   33,   32,
+       31,   17,   39,   39,   30,   28,   25,   24,   23,   22,
+       21,   20,   19,   18,   16,   15,   13,   10,    3,   38,
+       38,   38,   38,   38,   38,   38,   38,   38,   38,   38,
+       38,   38,   38,   38,   38,   38,   38,   38,   38,   38,
+       38,   38,   38
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -443,8 +461,14 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "ex1.l"
-#line 447 "lex.yy.c"
+#line 1 "myscanner.l"
+#line 2 "myscanner.l"
+    #include "myscanner.h"
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include "y.tab.h"
+#line 470 "lex.yy.c"
+#line 471 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -661,9 +685,10 @@ YY_DECL
 		}
 
 	{
-#line 1 "ex1.l"
+#line 8 "myscanner.l"
 
-#line 666 "lex.yy.c"
+
+#line 691 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -690,13 +715,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 18 )
+				if ( yy_current_state >= 39 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 19 );
+		while ( yy_base[yy_current_state] != 50 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -722,20 +747,56 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 2 "ex1.l"
-printf("GOODBYE\n");
+#line 10 "myscanner.l"
+return COLON;
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 3 "ex1.l"
-;
+#line 11 "myscanner.l"
+return TYPE;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 4 "ex1.l"
+#line 12 "myscanner.l"
+return NAME;
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 13 "myscanner.l"
+return TABLE_PREFIX;
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 14 "myscanner.l"
+return PORT;
+	YY_BREAK
+case 6:
+YY_RULE_SETUP
+#line 16 "myscanner.l"
+return IDENTIFIER;
+	YY_BREAK
+case 7:
+YY_RULE_SETUP
+#line 17 "myscanner.l"
+return INTEGER;
+	YY_BREAK
+case 8:
+/* rule 8 can match eol */
+YY_RULE_SETUP
+#line 18 "myscanner.l"
+;
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 19 "myscanner.l"
+printf("Unexpected character\n");
+	YY_BREAK
+case 10:
+YY_RULE_SETUP
+#line 21 "myscanner.l"
 ECHO;
 	YY_BREAK
-#line 738 "lex.yy.c"
+#line 799 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1032,7 +1093,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 18 )
+			if ( yy_current_state >= 39 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1060,11 +1121,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 18 )
+		if ( yy_current_state >= 39 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 17);
+	yy_is_jam = (yy_current_state == 38);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1740,6 +1801,39 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 4 "ex1.l"
+#line 21 "myscanner.l"
+
+
+int yywrap(void) 
+{
+    return 1;
+}
+
+int main(void) 
+{
+    int ntoken, vtoken;
+    ntoken = yylex();
+    while(ntoken)
+    {
+        printf("%d\n", ntoken);
+        printf("%s %d %s", yytext, yyleng, yylval);
+
+        /* if(yylex() != COLON)
+        {
+            printf("Syntax error in line %d, Expected a ':' but found %s\n", yylineno, yytext);
+            return 1;
+        }
+
+        vtoken = yylex();
+        switch(ntoken)
+        {
+
+        } */
+
+        ntoken = yylex();
+    }
+
+    return 0;
+}
 
 
